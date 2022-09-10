c2dd9ab352efd22d70307debdb573182625d849f
henriqueneubert <neubert.study@gmail.com>
2022-09-10 14:48:22 -0300

    feat: adicionado funcoes para edicao de dados, css atualizado 0.3

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 import {validationInfo} from './lib/validation.js'; //! mudar para geral 
 // import {localstorage} from './lib/bd_localstorage.js'; 
 
+//Provisório
+
+function darkMode()
+{
+      
+}
+
+const mainForm = document.getElementById('formulario')
 const inputName = document.getElementById('name');
 const inputEmail = document.getElementById('email');
 const inputPhone = document.getElementById('phone');
 const inputMessage = document.getElementById('message');
 const inputCity = document.getElementById('city');
 const inputCheckboxA = document.getElementById('checkboxA');
+const inputSubmit = document.getElementById('submit');
+const inputIndex = document.getElementById('index');
+const listCheckbox = document.getElementById('check');
 
 function handleFormSubmit(event) 
 {
@@ -20,8 +31,55 @@ function handleFormSubmit(event)
       
       handleInsertLocalStorage(objectContactData)
 
-      handleDataList()
+      handleDataList()      
 }
+
+function handleFormSubmitUpdate(event)
+{      
+      event.preventDefault();
+
+      const objectContactData = getContactData()
+      
+      if(!validationInfo(objectContactData, false)){     
+            return false 
+      }
+
+      handleUpdateLocalStorage(objectContactData)
+
+      handleDataList()
+
+      handleStateForm('create')
+
+      setTimeout(function(){
+            alert('Contato ' + objectContactData.name + ' foi atualizado com sucesso')
+      }, 200)
+}
+
+function handleStateForm(state)
+{
+      if(state === 'create'){
+            mainForm.reset()
+            listCheckbox.style.display = 'block';
+            inputSubmit.value = 'ENVIAR'
+      
+            mainForm.addEventListener(
+                  'submit', handleFormSubmit, false  
+            );
+            mainForm.removeEventListener(
+                  'submit', handleFormSubmitUpdate, false  
+            );
+      }else if(state ==='update'){
+            listCheckbox.style.display = 'none';
+            inputSubmit.value = 'EDITAR'
+            mainForm.removeEventListener(
+                  'submit', handleFormSubmit, false // 
+            );
+            mainForm.addEventListener(
+                  'submit', handleFormSubmitUpdate, false // 
+            );
+      }
+}
+
 // Invoke function
 handleDataList()
 
@@ -34,14 +92,23 @@ function handleDataList()
             htmlList += 
             `<tr>
                   <td>
+                        ${index}
+                  </td>
+                  <td>
                         ${item.name}
                   </td>
                   <td>
                         ${item.date} - ${item.hour}
                   </td>
                   <td>
-                        <button index="${index}">
+                        <button class="show" index="${index}">
                               +
+                        </button>
+                        <button class="delete" index="${index}">
+                              -
+                        </button>
+                        <button class="edit" index="${index}">
+                              Editar
                         </button>
                   </td>
             </tr>`
@@ -49,21 +116,68 @@ function handleDataList()
       
       const tableBody = document.querySelector('tbody')
       tableBody.innerHTML = htmlList
-      const buttons = document.querySelectorAll('button')
-debugger
-      buttons.forEach(button => button.addEventListener('click', 
+      const buttonsShow = document.querySelectorAll('button.show')
+      const buttonsDelete = document.querySelectorAll('button.delete')
+      const buttonsEdit = document.querySelectorAll('button.edit')
+
+      buttonsShow.forEach(button => button.addEventListener('click', 
       detailItem))
+
+      buttonsDelete.forEach(button => button.addEventListener('click', 
+      deleteItem))
+
+      buttonsEdit.forEach(button => button.addEventListener('click', 
+      updateItem))
 } 
 
-function detailItem(elem)
+function updateItem(event)
 {
-      const index = elem.getAttribute('index')
+      const index = event.target.getAttribute('index')
+      const arr = getDataLocalstorage()
+      const item = arr[index]
+
+      window.location.href='#formulario';
+
+      inputName.value = item.name
+      inputPhone.value = item.phone
+      inputEmail.value = item.email
+      inputCity.value = item.city
+      inputMessage.value = item.message
+      inputIndex.value = index
+
+      
+      handleStateForm('update')
+      
+}
+
+function deleteItem(event)
+{
+      const index = event.target.getAttribute('index')
+      const arr = getDataLocalstorage()
+      const item = arr[index]
+
+      if(!confirm("Tem certeza que gostaria de excluir o contato " + item.name + '?')){
+            return false
+      }
+      
+      arr.splice(index, 1)
+      
+      insertLocalStorage(arr)
+      handleDataList()
+      setTimeout(function(){
+            alert('Contato ' + item.name + ' foi deletado da lista')
+      }, 200)
+}
+function detailItem(event)
+{      
+      const index = event.target.getAttribute('index')
 
       const arr = getDataLocalstorage()
       const item = arr[index]
       const details = 
       `
       Nome = ${item.name}
+      Mensagem = ${item.message}
       `
       alert(details)
 }
@@ -75,6 +189,19 @@ function handleInsertLocalStorage(objectContactData)
       arr.push(objectContactData)
       
       insertLocalStorage(arr)
+}
+
+function handleUpdateLocalStorage(objectContactData)
+{
+      const arr = getDataLocalstorage()
+      const newArray = arr.map((item, index)=>{            
+            if(index === parseInt(objectContactData.index)){                  
+                  return objectContactData
+            }
+            return item
+      })      
+      
+      insertLocalStorage(newArray)
 }
 
 function getDataLocalstorage()
@@ -101,13 +228,14 @@ function getContactData()
             email: inputEmail.value,
             city: inputCity.value,
             message: inputMessage.value,
+            index: inputIndex.value,
             checkboxA: true,
             date: new Date().toLocaleDateString(),
             hour: new Date().toLocaleTimeString(),
       }
 }
 
-document.getElementById('formulario').addEventListener(
+mainForm.addEventListener(
       'submit', handleFormSubmit, false // 
 );
 
